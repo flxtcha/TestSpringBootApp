0a1665f138754b217c34b2fd6e91a48231fcc015
Alexander Bloomfield <alex.bloomfield@version1.com>
2023-06-28 14:06:46 +0100

    Initial commit

---
+++ src/test/java/com/version1/TestSpringBootApp/TestSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.version1.TestSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
