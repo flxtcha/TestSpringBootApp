e33719a0e4e0bdead74ada242f316ed19e94be47
BloomfieldA <alexander.bloomfield@version1.com>
2023-07-17 13:09:41 +0100

    add second controller and repo with jpa

--- src/main/java/com/version1/TestSpringBootApp/controller/TestController.java
+++ src/main/java/com/version1/TestSpringBootApp/controller/TestController.java
@@ -1,21 +1,30 @@
 package com.version1.TestSpringBootApp.controller;
 
+import com.version1.TestSpringBootApp.model.Person;
+import com.version1.TestSpringBootApp.repository.PersonRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/test")
 public class TestController {
+
+    private final PersonRepository personRepository;
+
+    public TestController(PersonRepository personRepo) {
+        this.personRepository = personRepo;
+    }
 
     @GetMapping
     public String testController() {
         return "Hello Everyone";
     }
 
-    @GetMapping("/2")
-    public String test() {
-        return "2";
+    @PostMapping
+    public Person testPostController() {
+        return personRepository.save(new Person("Alex", "Bloomfield"));
     }
 
 }
