8e8aa4a7ec0b74ac165d5f2581aee11ae2573393
flxtcha <tom@jfletcher.me>
2023-08-16 10:05:06 +0100

    * Added Delete Person Service * Refactor - IF statements changed to switch case for handlecode in the PersonFactory

--- src/main/java/com/version1/TestSpringBootApp/service/PrintDetailsPersonService.java
+++ src/main/java/com/version1/TestSpringBootApp/service/PrintDetailsPersonService.java
@@ -4,7 +4,7 @@ import com.version1.TestSpringBootApp.model.Person;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PrintDetailsPersonService implements PersonService {
+public class PrintDetailsPersonService implements PersonService{
     @Override
     public void handlePersonData(Person person) {
         System.out.printf("Firstname: %s Surname: %s", person.getFirstName(), person.getSurname());
